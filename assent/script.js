9596007af8af10a36f3c70390cf09a2a90cd7123
Luis Cartaya <cartaya1@msn.com>
2021-06-24 17:19:16 -0400

    last upload

--- assent/script.js
+++ assent/script.js
@@ -56,19 +56,19 @@ else {
     alert("Invalid Password length")
     }    
 //----------------------------------------------------------------------------------------
-console.log(finalPassword);
-//----------------------------------------------------------------------------------------
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-//Write password to the #password input
+generateBtn.addEventListener("click", function(){
+});
+
+// Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+  var password = finalPassword;
+  var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
-
+  passwordText.value = password;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-console.log(writePassword);
-}
 //----------------------------------------------------------------------------------------
+//
